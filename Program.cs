d8468aae139d79d9fde3626962f1185ba58c82e1
abdulbasitkhandeveloper <basitkhanbasit640@gmail.com>
2025-02-05 20:04:06 +0500

    feat: add exception middleware
    Signed-off-by: abdulbasitkhandeveloper <basitkhanbasit640@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,8 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -29,7 +32,7 @@ app.UseCors(opt =>
 {
     opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
 });
-
+ 
 app.MapControllers();
 
 DbInitializer.initDb(app);
